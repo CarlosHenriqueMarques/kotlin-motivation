7c0bbda96d693fd35f127e878b019ee58d52f594
Carlos Maia <carlos.marques89@gmail.com>
2019-11-03 11:49:09 -0500

    Correção Splash and SharedPreference

--- app/src/main/java/com/example/motivation/SplashActivity.kt
+++ app/src/main/java/com/example/motivation/SplashActivity.kt
@@ -8,12 +8,13 @@ import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
-    private val mSecurity: SecurityPreferences = SecurityPreferences(this)
+    private lateinit var mSecurity : SecurityPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        mSecurity = SecurityPreferences(this)
         buttonSave.setOnClickListener {
             handleSave()
         }
