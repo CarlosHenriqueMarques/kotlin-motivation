2e9f0c71314055e4f0ce6e8b10d0e4b9445f1d68
Carlos Maia <carlos.marques89@gmail.com>
2019-11-03 21:56:30 -0500

    Tratamento filtro e lógica

--- app/src/main/java/com/example/motivation/view/MainActivity.kt
+++ app/src/main/java/com/example/motivation/view/MainActivity.kt
@@ -3,11 +3,42 @@ package com.example.motivation.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.motivation.R
+import com.example.motivation.util.MotivationConstant
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var mFilter : Int = MotivationConstant.PHRASE_FILTER.ALL
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadFilter()
+        handlerFilters()
+    }
+
+    private fun loadFilter() {
+        image_all.setImageResource(R.drawable.ic_all_select)
+    }
+
+    private fun handlerFilters() {
+        image_all.setImageResource(R.drawable.ic_all_unselect)
+        image_sun.setImageResource(R.drawable.ic_sun_unselect)
+        image_happy.setImageResource(R.drawable.ic_happy_unselected)
+
+        image_all.setOnClickListener {
+            mFilter = MotivationConstant.PHRASE_FILTER.ALL
+            image_all.setImageResource(R.drawable.ic_all_select)
+        }
+
+        image_sun.setOnClickListener {
+            mFilter = MotivationConstant.PHRASE_FILTER.SUN
+            image_sun.setImageResource(R.drawable.ic_sun_select)
+        }
+
+        image_happy.setOnClickListener {
+            mFilter = MotivationConstant.PHRASE_FILTER.HAPPY
+            image_happy.setImageResource(R.drawable.ic_happy_selected)
+        }
     }
 }
